07cb82b9e50782571c4781276c3807dd4aec9826
cshotton <cshotton@gmail.com>
2015-05-26 10:38:39 -0400

    more bot handler changes

--- routes/blog.js
+++ routes/blog.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
+function BotHandler (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Blogging a note from ' + userName + '!'
+  };
+ 
+  // avoid infinite loop
+  if (userName !== 'ChuckBot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+//  res.send('respond with a bot response');
+}
+
 /* GET bot response page. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a bot response');
-});
+router.get('/', BotHandler);
 
 /* POST bot response page. */
-router.post('/', function(req, res, next) {
-  res.send('respond with a bot response');
-});
+router.post('/', BotHandler);
 
 module.exports = router;
